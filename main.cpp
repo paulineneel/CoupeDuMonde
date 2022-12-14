b58bc9fd73d78b5b765771a4ef4d713fbe0bd4b2
paulineneel <pauline.neel@etu.sorbonne-universite.fr>
2022-12-14 16:33:43 +0100

    premier affichage de la carte du monde

---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <stdio.h>
+#include <SFML/OpenGL.hpp>
+
+
+int main(){
+
+    // Créez une fenêtre SFML
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Carte du monde");
+
+    // Charger l'image de la carte du monde en tant que texture
+    sf::Texture mapTexture;
+    if (!mapTexture.loadFromFile("world_map1.png")) 
+    {
+        //std::count << "Erreur : impossible de charger l'image de la carte du monde" << std::endl;
+        //return(1);
+    }
+
+    // Créez un sprite pour afficher la carte
+    sf::Sprite mapSprite;
+    mapSprite.setTexture(mapTexture);
+
+    // Boucle principale de jeu
+    while (window.isOpen()) {
+        // Traitez les événements
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+
+        // Effacer l'écran
+        window.clear();
+
+        // Dessinez la carte
+        window.draw(mapSprite);
+
+        //Afficher l'écran
+        window.display();
+
+    }
+}
