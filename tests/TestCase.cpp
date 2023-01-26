35bccddaac8251d787f244804ca7a749fc597120
paulineneel <pauline.neel@etu.sorbonne-universite.fr>
2023-01-26 18:59:45 +0100

    nouveaux tests unitaires

--- tests/TestCase.cpp
+++ tests/TestCase.cpp
@@ -3,16 +3,15 @@
 #include "catch.hpp"
 #include <iostream>
 #include <fstream>
-// #include "CoupeDeCheveux.h"
-// #include "CoupeDeCheveuxHommes.h"
-// #include "CoupeDeCheveuxFemmes.h"
-// #include "Coiffeur.h"
-// #include "CoiffeurSpecialisteHommes.h"
-// #include "CoiffeurSpecialisteFemmes.h"
+
 
 #include "/home/portable014/Bureau/neelp_1/MAIN_4_PC/Info/CoupeDuMonde/Coiffeur.h"
 #include "/home/portable014/Bureau/neelp_1/MAIN_4_PC/Info/CoupeDuMonde/CoiffeurSpecialisteFemmes.h"
 #include "/home/portable014/Bureau/neelp_1/MAIN_4_PC/Info/CoupeDuMonde/CoiffeurSpecialisteHommes.h"
+#include "/home/portable014/Bureau/neelp_1/MAIN_4_PC/Info/CoupeDuMonde/CoupeDeCheveux.h"
+#include "/home/portable014/Bureau/neelp_1/MAIN_4_PC/Info/CoupeDuMonde/CoupeDeCheveuxFemmes.h"
+#include "/home/portable014/Bureau/neelp_1/MAIN_4_PC/Info/CoupeDuMonde/CoupeDeCheveuxHommes.h"
+
 TEST_CASE("1 : Coiffeur Spécialiste Femme")
 {
     CoiffeurSpecialisteFemmes coiffeur("Pauline","Taillou","France","Ecotay");
@@ -32,4 +31,28 @@ TEST_CASE("2 : Coiffeur Spécialiste Homme")
     REQUIRE(coiffeur1.Coiffeur::getVille() == "Gretz");
 }
 
-//TEST_CASE("3 : ")
+TEST_CASE("3 : Coupe De Cheveux Femmes")
+{
+    CoupeDeCheveuxFemmes coupe1("court", "afro",20,0);
+    REQUIRE(coupe1.CoupeDeCheveux::getLongueur() == "court");
+    REQUIRE(coupe1.CoupeDeCheveux::getForme() == "afro");
+    REQUIRE(coupe1.CoupeDeCheveuxFemmes::getMeches() == 0);
+    REQUIRE(coupe1.CoupeDeCheveuxFemmes::getTarif() == 20);
+
+    coupe1.setMeches(1);
+    REQUIRE(coupe1.CoupeDeCheveuxFemmes::getMeches() == 1);
+    REQUIRE(coupe1.CoupeDeCheveuxFemmes::getTarif() == 30);
+}
+
+TEST_CASE("4 : Coupe De Cheveux Hommes")
+{
+    CoupeDeCheveuxHommes coupe2("court","degrade",15,1);
+    REQUIRE(coupe2.CoupeDeCheveux::getLongueur() == "court");
+    REQUIRE(coupe2.CoupeDeCheveux::getForme() == "degrade");
+    REQUIRE(coupe2.CoupeDeCheveuxHommes::getTarif() == 20);
+    REQUIRE(coupe2.CoupeDeCheveuxHommes::getBarbe() == 1);
+
+    coupe2.setBarbe(0);
+    REQUIRE(coupe2.CoupeDeCheveuxHommes::getTarif() == 15);
+    REQUIRE(coupe2.CoupeDeCheveuxHommes::getBarbe() == 0);
+}
